e04fe8db84686ee5f815769a4c203c18a1784234
Kurt Endless <kendis_it@hotmail.com>
2020-05-07 19:15:52 +0300

    update: change logic for a few shapes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Plot = require("./lib/Plot");
 
-const labels = ["test", "test2"];
-const data = [34, 54.3];
+const labels = ["test", "test2", "fdsf"];
+const data = [34, 54.3, 32.4];
 
 const plot = new Plot();
-plot.bar(labels, data);
-// plot.pie(labels, data);
-// plot.doughnut(labels, data);
+// plot.bar(labels, data);
+// plot.radar(labels, data);
+plot.polar(labels, data);
 
 
 // const test = new Plot();
