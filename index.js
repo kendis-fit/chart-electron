17d1fe70c2240f903322e0e7515c4600f23d0183
Kurt Endless <kendis_it@hotmail.com>
2020-05-07 22:00:50 +0300

    add: bubble plot

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const data = [34, 54.3, 32.4];
 const plot = new Plot();
 // plot.bar(labels, data);
 // plot.radar(labels, data);
-plot.polar(labels, data);
+// plot.pie(labels, data);
+// plot.show();
 
+console.log("fdfs");
 
-// const test = new Plot();
-// test.line(labels, [[...data], [10, 34]]);
+const test = new Plot();
+test.bubble(labels, [[{ x: 10, y: 34, r: 15}, { x: 43, y: 12, r: 65 }, { x: 43, y: 12, r: 65 }]]);
+test.show();
 
 // module.exports = { Plot };
