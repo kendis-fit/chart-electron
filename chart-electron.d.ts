813a8f215de4eb5b9a29d9cbd458ae2fb7ad3dd9
Kurt Endless <kendis_it@hotmail.com>
2020-05-09 19:35:57 +0300

    update: fixed bug with types

--- chart-electron.d.ts
+++ chart-electron.d.ts
@@ -1,32 +1,34 @@
-declare interface IPoint {
-    x: number;
-    y: number;
-    r: number;
-}
-
-declare class Plot {
+declare module "chart-electron" {
+    interface IPoint {
+        x: number;
+        y: number;
+        r: number;
+    }
     
-    constructor();
-
-    bar(labels: string[], data: number[]): void;
-
-    horizontalBar(labels: string[], data: number[]): void;
-
-    pie(labels: string[], data: number[]): void;
-
-    polar(labels: string[], data: number[]): void;
-
-    doughnut(labels: string[], data: number[]): void;
-
-    line(labels: string[], data: number[][]): void;
-
-    radar(labels: string[], data: number[][]): void;
-
-    groupedBar(labels: string[], data: number[]): void;
-
-    bubble(labels: string[], data: IPoint[][], colors:? string[]): void;
-
-    save(path: string): void;
-
-    show(): void;
+    class Plot {
+        
+        constructor();
+    
+        bar(labels: string[], data: number[]): void;
+    
+        horizontalBar(labels: string[], data: number[]): void;
+    
+        pie(labels: string[], data: number[]): void;
+    
+        polar(labels: string[], data: number[]): void;
+    
+        doughnut(labels: string[], data: number[]): void;
+    
+        line(labels: string[], data: number[][]): void;
+    
+        radar(labels: string[], data: number[][]): void;
+    
+        groupedBar(labels: string[], data: number[]): void;
+    
+        bubble(labels: string[], data: IPoint[][]): void;
+    
+        save(path: string): void;
+    
+        show(): void;
+    } 
 }
